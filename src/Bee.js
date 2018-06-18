47e2ce7bb4b1f20560ca59a188f789e080819e4e
Marcellino Ornelas <marcellino.ornelas.com>
2018-06-18 10:52:04 -0700

    finished Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,17 @@
-var Bee = function() {
+var Bee = function(age, job) {
+  Grub.call(
+    this,
+    age || 5, 
+    'yellow'
+  );
+
+  this.job = job || 'keep on growing';
 };
+
+Bee.prototype = Object.create( Grub.prototype );
+Bee.prototype.constructor = Bee;
+// should be
+// Object.defineProperty(Bee.prototype, 'constructor', {
+//   value: Bee
+// });
 
