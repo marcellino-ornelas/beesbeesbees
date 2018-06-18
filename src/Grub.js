675c946568bba94a7e04928bda195ea2c5a19b93
Marcellino Ornelas <marcellino.ornelas.com>
2018-06-18 10:51:46 -0700

    finished Grub

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,7 @@
-var Grub = function() {
+var Grub = function(age, color, food) {
+  this.age = age || 0;
+  this.color = color || 'pink';
+  this.food = food || 'jelly';
 };
 
+Grub.prototype.eat = function(){};
